3d88b24a3e3e8c4a8cf0572e1c15df313328c194
brahmatej-09 <gayambrahmatejreddy@gmail.com>
2026-08-04 10:11:09 +0530

    Day 3

--- Binary_search/BS_Upper_Bound.java
+++ Binary_search/BS_Upper_Bound.java
@@ -1,6 +1,6 @@
-package Arrays;
+package Binary_search;
 import java.util.*;
-public class BS_Lower_Bound {
+public class BS_Upper_Bound {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -8,18 +8,17 @@ public class BS_Lower_Bound {
         for(int i=0;i<n;i++) arr[i] = sc.nextInt();
         Arrays.sort(arr);
         int target = sc.nextInt();
-        int ans = lower(arr,target);
-        System.out.println(ans);
+        System.out.println(upper(arr,target));
         sc.close();
     }
-    public static int lower(int[] arr,int target) {
+    public static int upper(int[] arr,int target) {
         int n = arr.length;
         int hi = n-1;
         int lo = 0;
         int ans = n;
         while(lo<=hi) {
             int mid = lo + (hi-lo)/2;
-            if(arr[mid]>=target) {
+            if(arr[mid]>target) {
                     ans = mid;
                     hi = mid-1;
             }
--- Binary_search/Binary_Search.java
+++ Binary_search/Binary_Search.java
@@ -1,4 +1,4 @@
-package Arrays;
+package Binary_search;
 import java.util.*;
 public class Binary_Search {
     public static void main(String[] args) {
